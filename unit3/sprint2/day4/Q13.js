0b6a8f52a157531b3fa8dab6d173b7740922de4e
sp4rtanjena <jenaabhijeet16@gmail.com>
2024-10-25 17:50:59 +0530

    web204-u3-s2-d4-q13

---
+++ unit3/sprint2/day4/Q13.js
@@ -0,0 +1,6 @@
+let numValue = 42, stringValue = "42"
+
+console.log(numValue + stringValue)
+
+if ((numValue + stringValue) > 100) console.log("The result is greater than 100")
+else console.log("The result is less than or equal to 100")
