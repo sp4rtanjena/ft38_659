e30f8f97402c5c3fd1b889ae9f86cf5832657bc7
sp4rtanjena <jenaabhijeet16@gmail.com>
2024-10-25 17:50:18 +0530

    web204-u3-s2-d4-q12

---
+++ unit3/sprint2/day4/Q12.js
@@ -0,0 +1,22 @@
+let num1 = 20, num2 = 10
+
+// Addition
+let add = num1 + num2
+console.log(add)
+
+// Subtraction
+let sub = num1 - num2
+console.log(sub)
+
+// Multiplication
+let mul = num1 * num2
+console.log(mul)
+
+// greater than
+console.log(num1 > num2)
+
+// equal 
+console.log(num1 == num2)
+
+// Strictly equal
+console.log(num1 === num2)
